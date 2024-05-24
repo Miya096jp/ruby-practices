2b21dc467fd9bde5d2f15341671e3b5b16f742a4
Miya096jp <miyawaki.yske@gmail.com>
2024-05-24 13:20:57 +0900

    カレンダー再修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -14,22 +14,21 @@ month = options["m"] ? options["m"].to_i : default_month
 first_day = Date.new(year, month, +1)
 last_day = Date.new(year, month, -1)
 
-days_and_weekdays = (first_day..last_day).map do |date_object|
-  [date_object.day.to_s.rjust(2), date_object.wday]
-end
-
-first_weekday = days_and_weekdays[0][1]
-first_weekday.times {days_and_weekdays.unshift(["  ", "x"])}
-
 header = "#{month}月 #{year}"
 puts header.center(20)
 
-print "日 月 火 水 木 金 土\n"
-days_and_weekdays.each do |day, weekday|
-  if weekday == 6
-    all_days = day + "\n"
+(first_day..last_day).each do |date|
+  if date.day == 1
+    x = date.wday
+    if date.wday == 6
+      all_days = "   " * x + " 1\n"
+    else
+      all_days = "   " * x + " 1 "
+    end
+  elsif date.wday == 6
+    all_days = date.day.to_s.rjust(2) + "\n"
   else
-    all_days = day + " "
+    all_days = date.day.to_s.rjust(2) + " "
   end
   print all_days
 end
