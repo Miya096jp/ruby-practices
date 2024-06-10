b0a9b1730d01b6dcfe1a111f4a5b1d4c0d6a38d1
Miya096jp <miyawaki.yske@gmail.com>
2024-06-10 09:32:31 +0900

    修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -5,57 +5,48 @@
 score = ARGV[0]
 scores = score.split(',')
 
+FULL_SCORE = 10
+
 shots = []
 scores.map do |s|
   if s == 'X'
-    shots << 10
+    shots << FULL_SCORE
     shots << 0
   else
     shots << s.to_i
   end
 end
 
-frames = []
-shots.each_slice(2) do |s|
-  frames << s
-end
+frames = shots.each_slice(2).to_a
 
 frames[9..] = [frames[9..].each do |frame|
-  frame.delete_at 1 if frame[0] == 10
+  frame.delete_at 1 if frame[0] == FULL_SCORE
 end.flatten]
 
 point = 0
 
-def strike_shot(frames, frame, idx)
+def strike_shot(frames, idx)
   case idx
   when 0..7
-    frames[idx + 1][0] == 10 ? 20 + frames[idx + 2][0] : 10 + frames[idx + 1].sum
+    frames[idx + 1][0] == FULL_SCORE ? 20 + frames[idx + 2][0] : FULL_SCORE + frames[idx + 1].sum
   when 8
-    frame[0] + frames[9][0..1].sum
+    frames[idx][0] + frames[9][0..1].sum
   else
-    frame.sum
+    frames[idx].sum
   end
 end
 
-def score_shot(frames, frame, idx)
-  frame.sum + frames[idx + 1][0]
-end
-
-def normal_shot(frame, idx)
-  if idx == 9
-    frame.sum
-  elsif frame.sum != 10
-    frame.sum
-  end
+def score_shot(frames, idx)
+  frames[idx].sum + frames[idx + 1][0]
 end
 
 frames.each.with_index do |frame, idx|
-  point += if frame[0] == 10
-             strike_shot(frames, frame, idx)
-           elsif frame.sum == 10 && frame[0] != 10
-             score_shot(frames, frame, idx)
+  point += if frame[0] == FULL_SCORE 
+             strike_shot(frames, idx)
+           elsif frame.sum == FULL_SCORE
+             score_shot(frames, idx)
            else
-             normal_shot(frame, idx)
+             frame.sum
            end
 end
 
