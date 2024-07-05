9d1be61a1a06d2a671ca6272591bab3ff79dfdd2
Miya096jp <miyawaki.yske@gmail.com>
2024-07-05 23:30:51 +0900

    lsコマンド4修正1

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -5,6 +5,10 @@
 require 'optparse'
 require 'etc'
 COLUMN = 3
+CONVERT_FILETYPE = { '1' => 'p', '2' => 'c', '4' => 'd', '6' => 'b', '10' => '-', '12' => 'l', '14' => 's' }.freeze
+NORMAL_PERMISSION = { '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx' }.freeze
+SUID_OR_SGID = { '0' => '---', '1' => '--s', '2' => '-wS', '3' => '-ws', '4' => 'r-S', '5' => 'r-s', '6' => 'rwS', '7' => 'rws' }.freeze
+STICKY_BIT = { '0' => '---', '1' => '--t', '2' => '-wT', '3' => '-wt', '4' => 'r-T', '5' => 'r-t', '6' => 'rwT', '7' => 'rwt' }.freeze
 all_entries = Dir.entries('.').sort
 
 options = ARGV.getopts('l')
@@ -38,19 +42,19 @@ transposed_entries = transpose_entries(filtered_entries, COLUMN, row)
 # ここより以下はlオプションの実装
 
 def convert_filetype(filetype)
-  { '1' => 'p', '2' => 'c', '4' => 'd', '6' => 'b', '10' => '-', '12' => 'l', '14' => 's' }[filetype]
+  CONVERT_FILETYPE[filetype]
 end
 
 def normal_permission(user_type)
-  { '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx' }[user_type]
+  NORMAL_PERMISSION[user_type]
 end
 
 def suid_or_sgid(user_type)
-  { '0' => '---', '1' => '--s', '2' => '-wS', '3' => '-ws', '4' => 'r-S', '5' => 'r-s', '6' => 'rwS', '7' => 'rws' }[user_type]
+  SUID_OR_SGID[user_type]
 end
 
-def stickey_bit(user_type)
-  { '0' => '---', '1' => '--t', '2' => '-wT', '3' => '-wt', '4' => 'r-T', '5' => 'r-t', '6' => 'rwT', '7' => 'rwt' }[user_type]
+def sticky_bit(user_type)
+  STICKY_BIT[user_type]
 end
 
 def format_filetype_and_mode(file_stats)
@@ -70,7 +74,7 @@ def format_filetype_and_mode(file_stats)
       normal_permission(owner) + normal_permission(group) + sticky_bit(other)
     when '2'
       suid_or_sgid(owner) + normal_permission(group) + normal_permission(other)
-    when '3'
+    when '4'
       normal_permission(owner) + suid_or_sgid(group) + normal_permission(other)
     end
 end
@@ -99,36 +103,20 @@ def convert_file_stats_into_strings(file_stats, entry_names)
 end
 
 def total_blocks(file_stats)
-  file_stats.map { |file| file.blocks.to_i }.sum
+  file_stats.map.sum { |file| file.blocks.to_i }
 end
 
 file_stats = filtered_entries.map { |entry| File.stat(entry) }
+
 converted_file_stats = convert_file_stats_into_strings(file_stats, filtered_entries)
 total_block_to_display = total_blocks(file_stats)
 
 transposed_file_stats = converted_file_stats.transpose
-longest_link = transposed_file_stats[1].max_by(&:length).length + 1
-longest_user = transposed_file_stats[2].max_by(&:length).length + 1
-longest_owner = transposed_file_stats[3].max_by(&:length).length + 1
-longest_size = transposed_file_stats[4].max_by(&:length).length + 1
-longest_filename = transposed_file_stats[6].max_by(&:length).length + 1
 
-file_stats_to_display = converted_file_stats.each do |formatted_file_stat|
-  formatted_file_stat.each_with_index do |status, index|
-    case index
-    when 1
-      formatted_file_stat[index] = status.rjust(longest_link)
-    when 2
-      formatted_file_stat[index] = status.rjust(longest_user)
-    when 3
-      formatted_file_stat[index] = status.rjust(longest_owner)
-    when 4
-      formatted_file_stat[index] = status.rjust(longest_size)
-    when 6
-     formatted_file_stat[index] = status.rjust(longest_filename)
-    end
-  end << "\n"
-end.join
+max_lengths = transposed_file_stats.map.with_index { |file_stat, index| index != 0 ? file_stat.max_by(&:length).length + 1 : file_stat.max_by(&:length).length }
+
+file_stats_to_display =
+  transposed_file_stats.map.with_index { |file_stat, index| file_stat.map { _1.rjust(max_lengths[index]) } }.transpose.map(&:join).join("\n")
 
 if options['l']
   puts "total #{total_block_to_display}"
