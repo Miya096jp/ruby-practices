c404523daa49e74f3fcdd9bf5ac09df2f84244c9
Miya096jp <miyawaki.yske@gmail.com>
2024-07-26 08:47:42 +0900

    全般的に作り直し完了

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -5,84 +5,82 @@
 require 'optparse'
 require 'debug'
 
-def exec
-  opt = OptionParser.new
-  params = { count_lines: false, count_words: false, count_bytes: false }
-  opt.on('-l') { |v| params[:count_lines] = v }
-  opt.on('-w') { |v| params[:count_words] = v }
-  opt.on('-c') { |v| params[:count_bytes] = v }
-  opt.parse!(ARGV)
+opt = OptionParser.new
+params = { count_lines: false, count_words: false, count_bytes: false }
+opt.on('-l') { |v| params[:count_lines] = v }
+opt.on('-w') { |v| params[:count_words] = v }
+opt.on('-c') { |v| params[:count_bytes] = v }
+opt.parse!(ARGV)
 
-  ARGV[0] ? process_files(ARGV, **params) : process_stdin(ARGF, **params)
-end
+file_names = ARGV
 
-def process_files(file_names, **params)
-  file_texts = read_files(file_names)
-  statistics = compute_statistics(file_texts, **params)
-  if file_names.size >= 2
-    statistics_with_total = culculate_total(statistics)
-    padded_statistics = pad_statistics(statistics_with_total)
-  else
-    padded_statistics = pad_statistics(statistics)
-  end
-  statistics_and_file_names = append_names(padded_statistics, file_names)
-  puts render_statistics(statistics_and_file_names)
-end
+file_texts = if file_names[0]
+               file_names.map { |file_name| File.open(file_name, 'r').read }
+             else
+               [ARGF.readlines.join]
+             end
 
-def read_files(file_names)
-  file_names.map { |file_name| File.open(file_name, 'r').read }
-end
-
-def culculate_total(statistics)
-  total_statistics = statistics[0].zip(*statistics[1..]).map(&:sum)
-  statistics << total_statistics
-end
-
-def pad_statistics(statistics)
-  padding_list = compute_paddings(statistics)
-  statistics.map do |statistic|
-    statistic.map.with_index { |element, idx| element.to_s.rjust(padding_list[idx]) }
-  end
-end
-
-def compute_paddings(statistics)
-  max_lengths = statistics[0].zip(*statistics[1..]).map do |each_statistic|
-    each_statistic.map(&:to_s).max_by(&:length).length
-  end
-  max_lengths.map { |max_length| max_length >= 8 ? max_length : 8 }
-end
-
-def append_names(padded_statistics, file_names)
-  padded_statistics.map.with_index do |padded_statistic, idx|
-    padded_statistic << (file_names[idx] || 'total')
-  end
-end
-
-def process_stdin(argf, **params)
-  statistics = compute_statistics([argf.readlines.join], **params)
-  padded_statistics = pad_statistics(statistics)
-  puts render_statistics(padded_statistics)
-end
-
-def compute_statistics(file_texts, count_lines: false, count_words: false, count_bytes: false)
+def get_text_counts(file_texts)
   file_texts.map do |file_text|
-    array = []
-    array << file_text.count("\n") if count_lines == true
-    array << file_text.split(/\s+/).count if count_words == true
-    array << file_text.to_s.bytesize if count_bytes == true
-    array = default_set(file_text) if array.empty?
-    array
+    {
+      lines: file_text.count("\n"),
+      words: file_text.split(/\s+/).count,
+      bytes: file_text.to_s.bytesize
+    }
   end
 end
 
-def default_set(file_text)
-  [file_text.count("\n"), file_text.split(/\s+/).count, file_text.to_s.bytesize]
+text_counts = get_text_counts(file_texts)
+
+def get_total(text_counts)
+  text_counts << text_counts.each_with_object({}) do |text_count, total|
+    text_count.each do |key, value|
+      total[key] ||= 0
+      total[key] += value
+    end
+  end
 end
 
-def render_statistics(statistics)
-  statistics.map do |statistic|
-    statistic.join(' ')
+def format_text_counts(text_counts, file_names)
+  keys = text_counts.first.keys
+  max_length = keys.map do |key|
+    text_counts.map { |text_count| text_count[key].to_s.size }.max
+  end
+  max_lengths = keys.zip(max_length).to_h
+
+  text_counts.map.with_index do |text_count, idx|
+    {
+      lines: text_count[:lines].to_s.rjust(max_lengths[:lines] >= 7 ? max_lengths[:lines] : 7),
+      words: text_count[:words].to_s.rjust(max_lengths[:words] >= 7 ? max_lengths[:words] : 7),
+      bytes: text_count[:bytes].to_s.rjust(max_lengths[:bytes] >= 7 ? max_lengths[:bytes] : 7),
+      name: text_count[:name] = file_names[idx]
+    }
+  end
+end
+
+if file_names.size >= 2
+  file_names << 'total'
+  get_total(text_counts)
+end
+
+text_counts = format_text_counts(text_counts, file_names)
+
+def render(text_counts, count_lines: false, count_words: false, count_bytes: false)
+  text_counts.map do |text_count|
+    elements = {
+      lines: text_count[:lines],
+      words: text_count[:words],
+      bytes: text_count[:bytes],
+      name: text_count[:name]
+    }
+
+    unless !count_lines && !count_words && !count_bytes
+      elements.delete(:lines) unless count_lines
+      elements.delete(:words) unless count_words
+      elements.delete(:bytes) unless count_bytes
+    end
+    elements.values.join(' ')
   end.join("\n")
 end
 
-exec
+puts render(text_counts, **params)
