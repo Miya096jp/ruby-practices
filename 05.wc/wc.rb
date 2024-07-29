1469b31597e14da79d9e17ed52572e9d53850caa
Miya096jp <miyawaki.yske@gmail.com>
2024-07-30 08:27:30 +0900

    第5回提出用

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -5,8 +5,6 @@
 require 'optparse'
 require 'debug'
 
-KEYS = %i[lines words bytes name].freeze
-
 opt = OptionParser.new
 options = { count_lines: false, count_words: false, count_bytes: false }
 opt.on('-l') { |v| options[:count_lines] = v }
@@ -14,14 +12,13 @@ opt.on('-w') { |v| options[:count_words] = v }
 opt.on('-c') { |v| options[:count_bytes] = v }
 opt.parse!(ARGV)
 
-keys_to_keep = []
+option_keys = []
 if !options[:count_lines] && !options[:count_words] && !options[:count_bytes]
-  keys_to_keep = %i[lines words bytes name]
+  option_keys = %i[lines words bytes]
 else
-  keys_to_keep << :lines if options[:count_lines]
-  keys_to_keep << :words if options[:count_words]
-  keys_to_keep << :bytes if options[:count_bytes]
-  keys_to_keep << :name
+  option_keys << :lines if options[:count_lines]
+  option_keys << :words if options[:count_words]
+  option_keys << :bytes if options[:count_bytes]
 end
 
 def get_text_counts(file_texts, file_names)
@@ -48,36 +45,28 @@ def get_total(text_counts)
   [total]
 end
 
-def get_max_lengths(text_counts, keys_to_keep)
-  max_lengths = keys_to_keep.map do |key|
+def get_max_lengths(text_counts, option_keys)
+  max_lengths = option_keys.map do |key|
     text_counts.map { |text_count| text_count[key].to_s.size }.max
   end
-  keys_to_keep.zip(max_lengths).to_h
+  option_keys.zip(max_lengths).to_h
 end
 
-def format_text_counts(text_counts, max_lengths, keys_to_keep)
+def render_lines(text_counts, max_lengths, option_keys)
   text_counts.map do |text_count|
     hash = {}
-    keys_to_keep.each do |key|
-      hash[key] = if key != :name
-                    text_count[key].to_s.rjust(max_lengths[key] >= 7 ? max_lengths[key] : 7)
-                  else
-                    text_count[:name]
-                  end
+    option_keys.each do |key|
+      hash[key] = text_count[key].to_s.rjust(max_lengths[key] >= 7 ? max_lengths[key] : 7)
     end
+    hash[:name] = text_count[:name]
     hash
   end
 end
 
-def render_text_counts(text_counts)
+def render_in_one_line(text_counts, max_lengths, option_keys)
+  text_counts = render_lines(text_counts, max_lengths, option_keys)
   text_counts.map do |text_count|
     text_count.values.join(' ')
-  end.join("\n")
-end
-
-def render_total_counts(total_counts)
-  total_counts.map do |total_count|
-    total_count.values.join(' ')
   end.join("\n")
 end
 
@@ -89,17 +78,7 @@ file_texts = if file_names[0]
              end
 
 text_counts = get_text_counts(file_texts, file_names)
-
-if file_names.size >= 2
-  total = get_total(text_counts)
-  max_lengths = get_max_lengths(total, keys_to_keep)
-
-  text_counts = format_text_counts(text_counts, max_lengths, keys_to_keep)
-  total_counts = format_text_counts(total, max_lengths, keys_to_keep)
-  puts render_text_counts(text_counts)
-  puts render_total_counts(total_counts)
-else
-  max_lengths = get_max_lengths(text_counts, keys_to_keep)
-  text_counts = format_text_counts(text_counts, max_lengths, keys_to_keep)
-  puts render_text_counts(text_counts)
-end
+total = get_total(text_counts)
+max_lengths = get_max_lengths(total, option_keys)
+puts render_in_one_line(text_counts, max_lengths, option_keys)
+puts render_in_one_line(total, max_lengths, option_keys) if file_names.size >= 2
